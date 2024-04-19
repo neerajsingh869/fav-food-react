42514cd032950e2bf0d2c1ef41bab4c4ca649b0c
Neeraj Singh <869neerajsingh@gmail.com>
2024-04-19 10:59:38 +0530

    feat: add data for food items

---
+++ src/data/Food.js
@@ -0,0 +1,27 @@
+export const food = {
+  pizza: {
+    name: "Pizza",
+    image:
+      "https://res.cloudinary.com/dl26pbek4/image/upload/v1672294935/assets/shourav-sheikh-a66sGfOnnqQ-unsplash_dufyvk.jpg",
+  },
+  burger: {
+    name: "Burger",
+    image:
+      "https://res.cloudinary.com/dl26pbek4/image/upload/v1672294934/assets/food-photographer-E94j3rMcxlw-unsplash_kuljtd.jpg",
+  },
+  pasta: {
+    name: "Pasta",
+    image:
+      "https://res.cloudinary.com/dl26pbek4/image/upload/v1672294934/assets/pixzolo-photography-aeESmmFKH0M-unsplash_wag5lc.jpg",
+  },
+  sushi: {
+    name: "Sushi",
+    image:
+      "https://res.cloudinary.com/dl26pbek4/image/upload/v1672294934/assets/pixzolo-photography-aeESmmFKH0M-unsplash_wag5lc.jpg",
+  },
+  chickenBiryani: {
+    name: "Chicken Biryani",
+    image:
+      "https://res.cloudinary.com/dl26pbek4/image/upload/v1672294935/assets/mario-raj-ysmeQt1dzcw-unsplash_ivdvre.jpg",
+  },
+};
