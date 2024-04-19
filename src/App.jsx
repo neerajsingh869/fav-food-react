c79dbf8fa7d0e308c98d50090640b3f0000487ec
Neeraj Singh <869neerajsingh@gmail.com>
2024-04-19 12:02:36 +0530

    style: use prettier to style page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css'
-import FoodItems from './components/FoodItems';
+import "./App.css";
+import FoodItems from "./components/FoodItems";
 
 function App() {
   return (
     <main className="p-8">
-      <h1 className="text-3xl font-semibold mb-8">Select your favourite food</h1>
+      <h1 className="text-3xl font-semibold mb-8">
+        Select your favourite food
+      </h1>
       <FoodItems />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -3,14 +3,13 @@
 const FoodCard = ({ item }) => {
   return (
     <div className="flex flex-col w-40 md:w-60 lg:w-80 p-4 pt-8 cursor-pointer bg-blue-200 rounded-xl">
-      <p className="font-semibold text-lg pb-2">
-        {item.name}
-      </p>
+      <p className="font-semibold text-lg pb-2">{item.name}</p>
       <div className="h-4/5">
         <img
           className="w-full h-full object-cover"
-          src={item.image} 
-          alt={item.name} />
+          src={item.image}
+          alt={item.name}
+        />
       </div>
     </div>
   );
