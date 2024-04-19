6c691ec19f222f881b521692866238f7e6e02fd9
Neeraj Singh <869neerajsingh@gmail.com>
2024-04-19 17:40:11 +0530

    style: add box-shadows

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -3,7 +3,7 @@
 const FoodCard = ({ item, handleFoodClick }) => {
   return (
     <div
-      className="flex flex-col w-40 md:w-60 lg:w-80 p-4 pt-8 cursor-pointer mb-6 bg-blue-200 rounded-xl"
+      className="flex flex-col w-40 md:w-60 lg:w-80 p-4 pt-8 duration-300 hover:shadow-2xl cursor-pointer mb-6 bg-blue-200 rounded-xl"
       onClick={() => handleFoodClick && handleFoodClick(item)}
     >
       <p className="font-semibold text-lg pb-2">{item.name}</p>
